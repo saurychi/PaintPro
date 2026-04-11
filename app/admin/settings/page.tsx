9fa2a139b0439bbe63492a68c02a048e55a68512
walter.caballero <21700254@usc.edu.ph>
2026-04-11 14:27:23 +0800

    refactor: changed setting's ui

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useMemo, useState } from "react"
 import { supabase } from "@/lib/supabaseClient"
 import { useRouter } from "next/navigation"
-import rawCountries from "@/app/data/country-by-calling-code.json"
+import rawCountries from "@/lib/data/country-by-calling-code.json"
 
 const ACCENT = "#00c065"
 const ACCENT_HOVER = "#00a054"
